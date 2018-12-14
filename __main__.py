cc85a38be0d054ce68a99df291a0340865800b26
Mokych Andrey <mokych.andrey@apriorit.com>
2018-12-14 11:12:20 +0200

    Extracted utils to separate files

--- __main__.py
+++ __main__.py
@@ -1,114 +1,29 @@
-from argparse import ArgumentParser, FileType
-import logging
-from logging import debug, info, warn
-from junitparser import JUnitXml, TestCase, Failure, Skipped, Error
-from testrail_api import APIClient
-from json import load
-from enum import Enum
 import re
+from argparse import ArgumentParser, FileType, Action
+from json import load
+from logging import DEBUG, INFO, debug, info
 
+from junitparser import JUnitXml, TestCase, TestSuite
 
-class LogFormatter(logging.Filter):
-    def filter(self, record):
-        labels_dict = {
-            logging.DEBUG: '[.]',
-            logging.INFO: '[+]',
-            logging.WARN: '[!]',
-            logging.ERROR: '[-]',
-            logging.FATAL:    '[ FATAL! ]',
-            logging.CRITICAL: '[CRITICAL]'
-        }
-        record.level_label = labels_dict[record.levelno]
-
-        return True
-
-
-def initialize_logger(loglevel=logging.INFO, **kwargs):
-    """
-    Create a logger with specified name and loglevel
-    """
-    logger = logging.getLogger()
-
-    logger.addFilter(LogFormatter())
-    syslog = logging.StreamHandler()
-    formatter = logging.Formatter('%(level_label)s %(message)s')
-    syslog.setFormatter(formatter)
-    logger.addHandler(syslog)
-    logger.setLevel(loglevel)
-
-
-def dump_report(options):
-    for report in options.reports:
-        try:
-            xml = JUnitXml.fromfile(report)
-            for suite in xml:
-                debug('Parsed testsuite: {}'.format(suite.name))
-                for case in suite:
-                    debug('Parsed testcase: {} [{}]'.format(
-                        case.name, case.result))
-                    if case.result is not None:
-                        debug('Result - {}: {}'.format(
-                            case.result.type, case.result.message))
-                        debug('Result - {}: {}'.format(
-                            case.result.type, case.result._elem.text))
-                    debug('Output: {} [{}]'.format(
-                        case.name, case.system_out))
-                    debug('Error: {} [{}]'.format(
-                        case.name, case.system_err))
-                info(suite.tests)
-                info(suite.failures)
-                info(suite.errors)
-        except Exception as error:
-            warn('Failed to parse report "{}". Error: {}'.format(report, error))
+from logging_utils import initialize_logger
+from results_formatter import push_results
+from testrail_api import APIClient
+from testrail_filter import (apply_case_filter, apply_section_filter,
+                             collect_children_sections, create_testrun)
 
 
 def main(options):
-    initialize_logger(
-        loglevel=logging.DEBUG if options.verbose else logging.INFO)
+    initialize_logger(loglevel=DEBUG if options.verbose else INFO)
     debug('Verbose logging is enabled')
-    mapping = load(options.mapping)
+    mapping = collect_full_mapping(options)
+    debug(mapping)
     project_id = mapping["project"]
     client = APIClient(project_id, url=options.server, user=options.username,
                        password=options.password)
 
-    testrun = None
     if not options.testrun:
-        debug('Loading test sections from TestRail...')
-        sections = client.get_sections()
-        filtered_sections = []
-        debug('Done!')
-        condition = mapping['filters']
-        if 'section' in condition:
-            filtered_sections = apply_section_filter(
-                sections, condition['section'])
-        debug('{} root sections are selected'.format(len(filtered_sections)))
-
-        sections_to_parse = []
-        for filtered_section in filtered_sections:
-            sections_to_parse += collect_children_sections(
-                sections, [filtered_section])
-            sections_to_parse += [filtered_section]
-
-        sections_ids = list(set([section['id']
-                                 for section in sections_to_parse]))
-        info('{} unique sections selected'.format(len(sections_ids)))
-
-        debug('Loading cases for selected sections...')
-        cases = [case for case in client.get_cases() if case['section_id']
-                 in sections_ids]
-
-        filtered_cases = []
-        debug('Loaded {} cases totally'.format(len(cases)))
-        if 'case' in condition:
-            filtered_cases = apply_case_filter(cases, condition['case'])
-
-        info('{} unique cases selected'.format(len(filtered_cases)))
-
-        debug('Creating new testrun for cases {}'.format(filtered_cases))
-        case_ids = [x['id'] for x in filtered_cases]
-
-        testrun = client.add_run(mapping['testrun']['name'],
-                                 mapping['testrun']['description'], case_ids)
+        info('Creating new test run since there is no testrun provided')
+        testrun = create_testrun(client, mapping)
     else:
         info('Loading testrun {}'.format(options.testrun))
         testrun = client.get_run(options.testrun)
@@ -120,13 +35,7 @@ def main(options):
 
     tests = client.get_tests(testrun_id)
 
-    test_results = []
-    for report in options.reports:
-        xml = JUnitXml.fromfile(report)
-        for suite in xml:
-            debug('Parsed testsuite: {}'.format(suite.name))
-            debug('Parsed {} test results'.format(len(suite)))
-            test_results += [case for case in suite]
+    test_results = read_test_results_from_report(options.reports)
 
     info('Mapping execution results to TestRail cases')
     mapped_results = map_results_to_cases(
@@ -135,6 +44,45 @@ def main(options):
     push_results(mapped_results, client)
     info('Test execution results are pushed to test run {}'.format(
         testrun['url']))
+
+
+def key_val_to_dict(keys, value, output_dict):
+    if keys:
+        try:
+            attr = output_dict[keys[0]]
+        except KeyError:
+            attr = None
+        debug('{}: {}'.format(keys[0], attr))
+        if type(attr) == dict:
+            output_dict[keys[0]] = key_val_to_dict(keys[1:], value, attr)
+        else:
+            output_dict[keys[0]] = value
+
+    return output_dict
+
+
+def collect_full_mapping(options):
+    original_mapping = load(options.mapping)
+    for keystring, value in options.defines.items():
+        keys = keystring.split('.')
+        original_mapping = key_val_to_dict(keys, value, original_mapping)
+    return original_mapping
+
+
+def read_test_results_from_report(reports):
+    test_results = []
+    for report in reports:
+        xml = JUnitXml.fromfile(report)
+        for suite in xml:
+            if type(suite) == TestSuite:
+                debug('Parsed testsuite: {}'.format(suite.name))
+                debug('Parsed {} test results'.format(len(suite)))
+                test_results += [case for case in suite]
+            elif type(suite) == TestCase:
+                debug('Parsed testsuite: {}'.format(suite.name))
+                debug('Parsed {} test results'.format(1))
+                test_results += [suite]
+    return test_results
 
 
 def map_results_to_cases(test_results, tests, mapping):
@@ -146,70 +94,6 @@ def map_results_to_cases(test_results, tests, mapping):
         }
 
     return results
-
-
-class TestStatus(Enum):
-    Passed = 1
-    Blocked = 2
-    Untested = 3
-    Retest = 4
-    Failed = 5
-
-
-def get_case_status(result: TestCase) -> TestStatus:
-    return {
-        'Failure': TestStatus.Failed,
-        'Error': TestStatus.Failed,
-        'Skipped': TestStatus.Untested,
-        'NoneType': TestStatus.Passed
-    }[result.result.__class__.__name__]
-
-
-def result_to_payload(result: TestCase):
-    payload = {}
-    payload['status_id'] = get_case_status(result).value
-    if payload['status_id'] != TestStatus.Untested:
-        payload['comment'] = """
-            Testsuite: {}
-            Testcase: 
-                {}
-            ===========================
-            Message: 
-                {}
-            Results details: 
-                {}
-            """.format(
-            result.classname,
-            result.name,
-            '{}: {}'.format(
-                result.result.type, result.result.message) if result.result is not None else 'Passed',
-            result.result._elem.text if result.result is not None else ''
-        )
-
-    return payload
-
-
-def push_results(results, testrail: APIClient):
-    total_count = len(results.items())
-    current_item = 0
-    submitted_results = 0
-    for test_id, related_info in results.items():
-        results = related_info['results']
-        test = related_info['test']
-        if results:
-            debug('Processing test "{}", found {} related result{}'.format(
-                test['title'], len(results), '' if len(results) == 1 else 's'))
-            for result in results:
-                payload = result_to_payload(result)
-                if payload['status_id'] != TestStatus.Untested.value:
-                    testrail.send_results(test_id, **payload)
-
-            submitted_results += 1
-        current_item += 1
-        debug('{}% cases processed'.format(
-            int((current_item / total_count) * 100)))
-    info('Submitted {} results to testrail. Coverage: {}%'.format(
-        submitted_results, int((submitted_results / total_count) * 100)))
 
 
 def get_results_for_test(test, results, mapping):
@@ -238,47 +122,16 @@ def merge_results(mapped_results, mapper):
     return merged
 
 
-def collect_children_sections(sections, filtered_sections):
-    results = []
-    for accepted_section in filtered_sections:
-        children = [
-            section for section in sections if section['parent_id'] == accepted_section['id']]
-
-        debug('There are {} children of {}'.format(
-            len(children), accepted_section['name']))
-        if children:
-            results += children
-            results += collect_children_sections(sections, children)
-
-    return results
-
-
-def apply_plain_filter(collection, condition):
-    results = []
-    for filter_key, filter_value in condition.items():
-        debug('Applying {} filter condition "{}"'.format(
-            filter_key, filter_value))
-        for item in collection:
-            if filter_key in item and item[filter_key] == filter_value:
-                results.append(item)
-
-    return results
-
-
-def apply_case_filter(cases, condition):
-    results = []
-    debug('Applying case filter...')
-    results = apply_plain_filter(cases, condition)
-    debug('Filtered {} cases to {}'.format(len(cases), len(results)))
-    return results
-
-
-def apply_section_filter(sections, condition):
-    results = []
-    debug('Applying section filter...')
-    results = apply_plain_filter(sections, condition)
-    debug('Filtered {} sections to {}'.format(len(sections), len(results)))
-    return results
+class StoreNameValuePair(Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        if not getattr(namespace, 'defines'):
+            setattr(namespace, 'defines', {})
+        defines = getattr(namespace, 'defines')
+        if type(values) != list:
+            values = [values]
+        for value in values:
+            k, v = value.split('=')
+            defines[k] = v
 
 
 if __name__ == "__main__":
@@ -323,6 +176,14 @@ if __name__ == "__main__":
         '-t', '--testrun',
         type=str,
         help='Existing testrun ID, if not exists - new one will be created'
+    )
+
+    parser.add_argument(
+        '-D', '--defines',
+        action=StoreNameValuePair,
+        nargs='+',
+        required=False,
+        help='Define mapping parameters in dynamic from commandline'
     )
 
     options = parser.parse_args()
